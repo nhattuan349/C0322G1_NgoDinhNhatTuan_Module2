7f943f4af159318dc7720cc51b24efc525271f12
unknown <nhattuanqlmt2@gmail.com>
2022-05-30 14:54:21 +0700

    update ss19

--- src/ss19String_Regex/BaiTap/CrawlTinTuc/CrawlNews.java
+++ src/ss19String_Regex/BaiTap/CrawlTinTuc/CrawlNews.java
@@ -2,10 +2,11 @@ package ss19String_Regex.BaiTap.CrawlTinTuc;
 import java.util.regex.Pattern;
 
 public class CrawlNews {
-    public static void main(String[] args) {
-        String regexCrawNews = "^(http?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-        String test = "http://dantri.com.vn/the-gioi.htm";
-        boolean checkRegex = Pattern.matches(regexCrawNews, test);
-        System.out.println(checkRegex);
+public static void main(String[] args) {
+    String regexCrawlNews = "^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+    String test = "https://james.codegym.vn/course/view.php?id=541&section=19";
+    boolean checkRegex = Pattern.matches(regexCrawlNews,test);
+    System.out.println(checkRegex);
+
     }
 }
