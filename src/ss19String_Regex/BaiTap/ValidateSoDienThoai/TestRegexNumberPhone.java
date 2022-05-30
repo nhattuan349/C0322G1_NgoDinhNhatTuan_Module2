bfd778079d1ed7fbdefc4d44e1baa3aa98a722ed
unknown <nhattuanqlmt2@gmail.com>
2022-05-30 15:14:21 +0700

    update ss19

--- src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
+++ src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
@@ -1,26 +1,18 @@
 package ss19String_Regex.BaiTap.ValidateSoDienThoai;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class TestRegexNumberPhone {
     public static void main(String[] args) {
         String testClassName = "^(C|P|A)[0-9]{4}(G|H|I|K|L|M)$";
-        String className1 = "C0123A";
-        String className2 = "A0123G";
-        String className3 = "@0123g";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập Tên của lớp học");
+        String className1 = scanner.nextLine();
+
         boolean check1 = Pattern.matches(testClassName, className1);
         if (check1) {
-            System.out.println("className -- " + className1 + "" + " --Hợp lệ");
-        } else System.out.println("className -- " + className1 + "" + " --không hợp lệ");
-
-        boolean check2 = Pattern.matches(testClassName, className2);
-        if (check2) {
-            System.out.println("className -- " + className2 + "" + " --Hợp lệ");
-        } else System.out.println("className -- " + className2 + "" + " --Không hợp lệ");
-
-        boolean check3 = Pattern.matches(testClassName, className3);
-        if (check3) {
-            System.out.println("className -- " + className3 + "" + " --Hợp lệ");
-        } else System.out.println("className -- " + className3 + "" + " --không hợp lệ");
+            System.out.println("className :" + className1 + "" + " --Khớp");
+        } else System.out.println("className :" + className1 + "" + " --không khớp");
     }
 }
