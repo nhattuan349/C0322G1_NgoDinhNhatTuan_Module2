623eebea45c1711687d8cfd1021d3bb3a84028c2
unknown <nhattuanqlmt2@gmail.com>
2022-05-30 15:35:06 +0700

    update ss19

--- src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
+++ src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
@@ -6,17 +6,15 @@ import java.util.regex.Pattern;
 public class TestRegexNumberPhone {
     public static void main(String[] args) {
         String phoneNumberRegex = "^(.)[0-9]{2}-(0)[0-9]{9}$";
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println(" Mời nhập số điện thoại ---- Ví dụ: +84-0899607659");
+            String str = scanner.nextLine();
 
-        String str = "+84-0899607659";
-        boolean check1 = Pattern.matches(phoneNumberRegex, str);
-        if (check1) {
-            System.out.println("Sđt: " + str + " --Hợp lệ");
-        } else System.out.println("Sđt: " + str + " --Không hợp lệ");
-
-        String str1 = "+84-95355535";
-        boolean check2 = Pattern.matches(phoneNumberRegex, str1);
-        if (check2) {
-            System.out.println("Sđt: " + str1 + " --Hợp lệ");
-        } else System.out.println("Sđt: " + str1 + " --Không hợp lệ");
+            boolean check = Pattern.matches(phoneNumberRegex, str);
+            if (check) {
+                System.out.println("sdt: " + str + " -->Khớp");
+            } else System.out.println("sdt: " + str + "Không khớpKhớp");
+        } while (true);
     }
 }
