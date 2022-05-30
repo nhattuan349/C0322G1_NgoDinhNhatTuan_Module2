d7e009c4ef92f0f9d9fac0ae400cfb978e3adc14
unknown <nhattuanqlmt2@gmail.com>
2022-05-30 15:19:48 +0700

    update ss19

--- src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
+++ src/ss19String_Regex/BaiTap/ValidateSoDienThoai/TestRegexNumberPhone.java
@@ -5,14 +5,16 @@ import java.util.regex.Pattern;
 
 public class TestRegexNumberPhone {
     public static void main(String[] args) {
-        String testClassName = "^(C|P|A)[0-9]{4}(G|H|I|K|L|M)$";
+        String testClassName = "^(C|P|A)[0-9]{4}(G|H|I|K|L|M)[0-9]{1}$";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập Tên của lớp học");
-        String className1 = scanner.nextLine();
+        do {
+            System.out.println("Nhập Tên của lớp học --- Ví dụ: C0322G1");
+            String className1 = scanner.nextLine();
 
-        boolean check1 = Pattern.matches(testClassName, className1);
-        if (check1) {
-            System.out.println("className :" + className1 + "" + " --Khớp");
-        } else System.out.println("className :" + className1 + "" + " --không khớp");
+            boolean check1 = Pattern.matches(testClassName, className1);
+            if (check1) {
+                System.out.println("className :" + className1 + "" + " --> Khớp");
+            } else System.out.println("className :" + className1 + "" + " --không khớp");
+        }while (true);
     }
 }
